54de22b4afd7e39ed881f342ea74524823fd7478
onyoo <roberto9m9@gmail.com>
2016-03-23 17:49:14 +0100

    convert VM to stock output

--- app/assets/javascripts/app/controllers/StockCtrl.js
+++ app/assets/javascripts/app/controllers/StockCtrl.js
@@ -1,13 +1,49 @@
 function StockCtrl () {
-  this.sayings = [{
-    hi: "hello",
-    bye: 'bye'
+  this.stocks = [{
+    name: "Apple Inc",
+    symbol: "AAPL",
+    lastPrice: 524.49,
+    change: 15.6,
+    changePercent: 3.06549549018453,
+    timestamp: "Wed Oct 23 13:39:19 UTC-06:00 2013",
+    mSDate:  41570.568969907,
+    marketCap: 476497591530,
+    volume: 397562,
+    changeYTD: 532.1729,
+    changePercentYTD: -1.44368493773359,
+    high: 52499,
+    low: 519.175,
+    open: 519.175
   },{
-    hi: "hola",
-    bye: 'adios'
+    name: "Apple Inc",
+    symbol: "AAPL",
+    lastPrice: 524.49,
+    change: 15.6,
+    changePercent: 3.06549549018453,
+    timestamp: "Wed Oct 23 13:39:19 UTC-06:00 2013",
+    mSDate:  41570.568969907,
+    marketCap: 476497591530,
+    volume: 397562,
+    changeYTD: 532.1729,
+    changePercentYTD: -1.44368493773359,
+    high: 52499,
+    low: 519.175,
+    open: 519.175
   },{
-    hi: "안녕하세요",
-    bye: '안녕히계세요'
+    name: "Apple Inc",
+    symbol: "AAPL",
+    lastPrice: 524.49,
+    change: 15.6,
+    changePercent: 3.06549549018453,
+    timestamp: "Wed Oct 23 13:39:19 UTC-06:00 2013",
+    mSDate:  41570.568969907,
+    marketCap: 476497591530,
+    volume: 397562,
+    changeYTD: 532.1729,
+    changePercentYTD: -1.44368493773359,
+    high: 52499,
+    low: 519.175,
+    open: 519.175
   }];
 }
 
