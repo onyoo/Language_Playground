374286df6e0a1407246c9765b4ef2cd4c6d23c72
onyoo <roberto9m9@gmail.com>
2016-03-26 16:07:21 +0100

    timer reset

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,5 @@
 angular
-  .module('app', ['ui.router', 'templates'])
+  .module('app', ['ui.router', 'templates', 'timer'])
   .config(function($stateProvider, $urlRouterProvider){
     $stateProvider
       .state('home', {
