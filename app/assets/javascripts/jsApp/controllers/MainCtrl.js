1626f8523a1f769e1d5ee30415dbe893baaf0d91
onyoo <roberto9m9@gmail.com>
2016-03-26 12:37:23 +0100

    document show page formatting

--- app/assets/javascripts/jsApp/controllers/MainCtrl.js
+++ app/assets/javascripts/jsApp/controllers/MainCtrl.js
@@ -1,8 +1,6 @@
 function MainCtrl (docService, $rootScope, $filter, $stateParams) {
 
   this.documents = docService;
-  // this.document = docService[$rootScope.$id];
-  this.id = $stateParams.id
 
   this.getDocs = function(){
     docService.getAll();
